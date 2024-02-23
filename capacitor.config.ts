e7b6c7e4d1fb9ade7772ff57eead3c0eda801188
dom <dominik.amrugiewicz@wohnsinn.com>
2024-02-23 12:55:38 +0100

    wip

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.tomodoro.app',
   appName: 'TomoDoro',
   webDir: 'dist',
   server: {
