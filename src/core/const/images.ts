dec52bfb8c768159e3b2e14617058264944b604f
dominikamrugiewicz <dominik.amrugiewicz@Wohnsinn.com>
2024-03-03 06:41:00 +0100

    ADD creativity xD

---
+++ src/core/const/images.ts
@@ -0,0 +1,11 @@
+export const IMAGES = {
+  leaf: <iImage>{
+    src: 'leaf.png',
+    alt: 'Green Leaf'
+  }
+};
+
+export interface iImage {
+  src: string;
+  alt: string;
+}
--- src/pages/TomoDoro.tsx
+++ src/pages/TomoDoro.tsx
@@ -1,14 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonImg, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { FC } from 'react';
 import Timer from '../components/Timer';
+import { IMAGES } from '../core/const/images';
 
 const TomoDoro: FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>TomoDoro</IonTitle>
-        </IonToolbar>
+        <IonImg style={{ height: 100 }} src={IMAGES.leaf.src} alt={IMAGES.leaf.alt} />
       </IonHeader>
       <IonContent fullscreen>
         <Timer />
