eb6a49d8135bdad9bc22dde2c36a702135616e87
dominikamrugiewicz <dominik.amrugiewicz@Wohnsinn.com>
2024-03-03 08:11:15 +0100

    refactor bmac image

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -48,7 +48,7 @@ const InfoPage: FC = () => {
                   <img
                     src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
                     alt="Buy Me A Coffee"
-                    // style={{ height: 60, width: 217 }}
+                    style={{ maxHeight: 80 }}
                   />
                 </a>
               </div>
